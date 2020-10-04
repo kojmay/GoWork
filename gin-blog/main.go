7ee96a5ed7d118ad5d1935c55a490e5c8a40b12a
Scofiled_Aliyun <haifengmay@163.com>
2020-10-04 23:51:29 +0800

    add .

--- gin-blog/main.go
+++ gin-blog/main.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"fmt"
-    "net/http"
-	
-	"github.com/kojmay/GoWork/gin-blog/routers"
+	"net/http"
+
+	"github.com/kojmay/GOWORK/gin-blog/routers"
 	"github.com/kojmay/GoWork/gin-blog/pkg/setting"
 )
 
 func main() {
-    router := routers.InitRouter()
-    s := &http.Server{
-        Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-        Handler:        router,
-        ReadTimeout:    setting.ReadTimeout,
-        WriteTimeout:   setting.WriteTimeout,
-        MaxHeaderBytes: 1 << 20,
-    }
-    s.ListenAndServe()
+	router := routers.InitRouter()
+
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
 }
--- gin-blog/routers/router.go
+++ gin-blog/routers/router.go
@@ -1,32 +1,30 @@
-package routers 
+package routers
 
 import (
+	// v1 "github.com/EDDYCJY/go-gin-example/routers/api/v1"
 	"github.com/gin-gonic/gin"
+
+	v1 "github.com/kojmay/GOWORK/gin-blog/routers/api/v1"
 	"github.com/kojmay/GoWork/gin-blog/pkg/setting"
-	"github.com/kojmay/GoWork/gin-blog/routers/api/v1"
 )
 
 func InitRouter() *gin.Engine {
+
 	r := gin.New()
 
 	r.Use(gin.Logger())
+
 	r.Use(gin.Recovery())
+
 	gin.SetMode(setting.RunMode)
 
-	apiv1 := r.Group("/api/v1") 
+	apiv1 := r.Group("/api/v1")
 	{
-		//获取标签列表
-        apiv1.GET("/tags", v1.GetTags)
-        //新建标签
-        apiv1.POST("/tags", v1.AddTag)
-        //更新指定标签
-        apiv1.PUT("/tags/:id", v1.EditTag)
-        //删除指定标签
-        apiv1.DELETE("/tags/:id", v1.DeleteTag)
+		apiv1.GET("/tags", v1.GetTags)
+		apiv1.POST("/tags", v1.AddTag)
+		apiv1.PUT("/tags/:id", v1.EditTag)
+		apiv1.DELETE("/tags/:id", v1.DeleteTag)
 	}
-	
 
-
-    return r
-
+	return r
 }
